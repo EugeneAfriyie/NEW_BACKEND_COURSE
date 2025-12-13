8f758bad257bfc613eb6b4a2b9c4124bc1558d1e
Eugene Afriyie <groupeight00@gmail.com>
2025-12-13 20:23:46 +0000

    starting woth rest api

---
+++ My_learning/REST-API-DEVELOPMENT/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+// middleware
+app.use(express.json());
+
+let books = [
+    { id: 1, title: '1984', author: 'George Orwell' },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
+]
+

60b082468221f1dd30a89cd314c18ba82957ac8b
Eugene Afriyie <groupeight00@gmail.com>
2025-12-14 10:24:20 +0000

    done with the basic rest api

--- My_learning/REST-API-DEVELOPMENT/app.js
+++ My_learning/REST-API-DEVELOPMENT/app.js
@@ -13,3 +13,77 @@ let books = [
     { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
 ]
 
+//intro routes
+app.get('/', (req,res) =>{
+    res.json({ message: 'Welcome to the Book API' });
+})
+
+// get all books 
+app.get('/getbooks', (req,res) =>{
+    res.json(books)
+})
+
+
+//  get a single book
+
+app.get('/getbooks/:id', (req, res) =>{
+    const bookId = parseInt(req.params.id);
+    const book = books.find(b => b.id === bookId)
+
+    if (book){
+        res.json(book);
+    }else{
+        res.status(404).json({ message: 'Book not found'});
+
+    }
+})
+
+// add a new book
+app.post('/addbook', (req,res)=>{
+    const newbook = {
+        id: books.length + 1,
+        title: `Book ${books.length + 1}`,
+        author: "Eugene"
+}
+
+books.push(newbook);
+
+res.status(201).json(newbook);
+res.json({ message: 'Book added successfully' });
+res.json(books);
+})
+
+// Update book 
+app.put('/updatebook/:id', ( req,res) =>{
+    const bookId = parseInt(req.params.id)
+    const book = books.find(b => b.id === bookId )
+
+    if (!book){
+        return res.status(404).json({ message: 'Book not found'});
+    }
+
+    if (req.body.title) book.title = req.body.title;
+    if (req.body.author) book.author = req.body.author;
+
+    res.json({message: 'Book updated successfully', book, books})
+})
+
+
+// delete a book
+app.delete('/deletebook/:id', (req,res) =>{
+    const bookId = parseInt(req.params.id);
+    const bookIndex = books.findIndex(b => b.id === bookId);
+
+    if (bookIndex === -1){
+        return res.status(404).json({ message: 'Book not found'});
+    }
+
+    books.splice(bookIndex, 1);
+
+    res.json({ message: 'Book deleted successfully', bookId,bookIndex });
+})
+
+
+app.listen(port, () =>{
+    console.log(`Server is running on http://localhost:${port}`);
+});

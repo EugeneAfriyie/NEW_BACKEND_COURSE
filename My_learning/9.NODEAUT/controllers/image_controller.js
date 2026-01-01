811f9afd2012033e5b31f8524ea8bc14821ff9f8
Eugene Afriyie <groupeight00@gmail.com>
2026-01-01 23:59:22 +0000

    Add fetch all images functionality and improve response handling

--- My_learning/9.NODEAUT/controllers/image_controller.js
+++ My_learning/9.NODEAUT/controllers/image_controller.js
@@ -1,6 +1,6 @@
  const Image = require("../models/image");
  const {uploadToCloudinary} = require("../helpers/cloudinaryHelper")
-
+const fs = require("fs");
 
  const uploadImageController = async (req,res)  =>{
     try {
@@ -26,6 +26,12 @@
         })
         await newlyUploadedImage.save();
 
+        // delete the file from local storage
+        fs.unlinkSync(req.file.path);
+
+        // response to client   
+
+
         res.status(201).json({
             success: true,
             message: "Image uploaded successfully",
@@ -41,4 +47,29 @@
     }
  }
 
-    module.exports = uploadImageController;
+
+ const fetchAllImagesController = async (req,res) =>{
+    try {
+        const images = await Image.find({});
+        if(images.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: "No images found"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            images
+        });
+    } catch (error) {
+        console.error("Error fetching images:", error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error" 
+        });
+    }
+ }
+
+
+
+    module.exports = {uploadImageController,fetchAllImagesController};
--- My_learning/9.NODEAUT/routes/imageRoutes.js
+++ My_learning/9.NODEAUT/routes/imageRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const authMiddleware = require('../Middleware/authMiddleware');
 const adminMiddleware = require('../Middleware/adminMiddleware');
 const uploadMiddleware = require('../Middleware/uploadMiddleware');
-const uploadImageController = require('../controllers/image_controller');
+const { uploadImageController, fetchAllImagesController } = require('../controllers/image_controller');
 
 
 // Upload the img 
 router.post("/upload",authMiddleware,adminMiddleware,uploadMiddleware.single("image"),uploadImageController);
+
+// fetch all images
+router.get("/fetchall",authMiddleware,adminMiddleware,fetchAllImagesController);
 
 module.exports.uploadImageRoutes = router;

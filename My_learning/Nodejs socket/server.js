6358ee4b300b2ca6eaa8abc523378713bec58559
Eugene Afriyie <groupeight00@gmail.com>
2026-01-10 23:18:44 +0000

    Add user disconnection handling and chat message broadcasting

--- My_learning/Nodejs socket/server.js
+++ My_learning/Nodejs socket/server.js
@@ -1,4 +1,5 @@
- const express = require("express")
+ const { Socket } = require("dgram")
+const express = require("express")
  const http = require("http")
 
  const socketio = require("socket.io")
@@ -20,6 +21,7 @@
             // handle us er when they will join the chat room
             socket.on("join", (userName) =>{
                 users.add(userName)
+                socket.userName = userName
 
                 // Broadcast to all client/users that new user has join
 
@@ -33,9 +35,31 @@
 
 
             // handle incoming chat messages
+            socket.on("chatMessage", (message) =>{
+                // broadcast he recent message to all users
+                io.emit("chatMessage", message)
+            })
 
 
             // handle user disconnection
+
+            socket.on("disconnect", () =>{
+              console.log("User id disconnected", socket.id)
+
+              users.forEach(user => {
+                if(user === socket.userName){
+                    users.delete(user)
+
+                    io.emit("userLeft", user)
+
+                    // send the updated user list to all users
+                    io.emit("userList", Array.from(users))
+                }
+
+              });
+              })
+
+                
 
 
 
